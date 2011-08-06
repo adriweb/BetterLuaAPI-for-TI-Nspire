58748924f5ebd909f7211b03f9a9718e888d8544
Adrien Bertrand <adrienbertrand@msn.com>
2011-08-06 13:04:38 -0500

    Lots of stuff changed, mainly the color handling
    Changed all the platform.gc() by a global variable, "myGC", which is the
    same as the gc pass to the on.paint event. Added function clearWindow
    Redid setColor() function from scratch.  It can now handle strings, for
    example : setColor("red"), and direct colors, for example:
    setColor({255,0,0})

--- BetterLuaAPI.lua
+++ BetterLuaAPI.lua
@@ -1,6 +1,41 @@
 -- BetterLuaAPI for TI-Nspire
+-- Version 2.0 (Aug 6th 2011)
 -- Adriweb 2011
 -- Put all this or some of this (be careful, though, some functions use each other) in your code and thank me ;)
+-- Remember to put "myGC = gc" in the  on.paint(gc)  function.
+
+
+Color = {
+	["black"] = {0, 0, 0},
+	["red"] = {0xff, 0, 0},
+	["green"] = {0, 0xff, 0},
+	["blue "] = {0, 0, 0xff},
+	["white"] = {0xff, 0xff, 0xff},
+	["brown"] = {165,42,42},
+	["cyan"] = {0,255,255},
+	["darkblue"] = {0,0,139},
+	["darkred"] = {139,0,0},
+	["fuchsia"] = {255,0,255},
+	["gold"] = {255,215,0},
+	["gray"] = {127,127,127},
+	["grey"] = {127,127,127},
+	["lightblue"] = {173,216,230},
+	["lightgreen"] = {144,238,144},
+	["magenta"] = {255,0,255},
+	["maroon"] = {128,0,0},
+	["navyblue"] = {159,175,223},
+	["orange"] = {255,165,0},
+	["palegreen"] = {152,251,152},
+	["pink"] = {255,192,203},
+	["purple"] = {128,0,128},
+	["royalblue"] = {65,105,225},
+	["salmon"] = {250,128,114},
+	["seagreen"] = {46,139,87},
+	["silver"] = {192,192,192},
+	["turquoise"] = {64,224,208},
+	["violet"] = {238,130,238},
+	["yellow"] = {255,255,0}
+}
 
 function copyTable(t)
   local t2 = {}
@@ -49,94 +84,79 @@ function pwh()
 end
 
 function drawPoint(x, y)
-	platform.gc():fillRect(x, y, 1, 1)
+	myGC:fillRect(x, y, 1, 1)
 end
 
 function drawCircle(x, y, diameter)
-	platform.gc():drawArc(x - diameter/2, y - diameter/2, diameter,diameter,0,360)
+	myGC:drawArc(x - diameter/2, y - diameter/2, diameter,diameter,0,360)
 end
 
 function drawCenteredString(str)
-	platform.gc():drawString(str, (pww() - platform.gc():getStringWidth(str)) / 2, pwh() / 2, "middle")
+	myGC:drawString(str, (pww() - myGC:getStringWidth(str)) / 2, pwh() / 2, "middle")
 end
 
 function setColor(theColor)
-	theColor = string.lower(theColor)
-	platform.gc():setColorRGB(0,0,0) -- set black as default is nothing else valid is found
-	if theColor == "blue" then platform.gc():setColorRGB(0,0,255)
-	elseif theColor == "brown" then platform.gc():setColorRGB(165,42,42)
-	elseif theColor == "cyan" then platform.gc():setColorRGB(0,255,255)
-	elseif theColor == "darkblue" then platform.gc():setColorRGB(0,0,139)
-	elseif theColor == "darkred" then platform.gc():setColorRGB(139,0,0)
-	elseif theColor == "fuchsia" then platform.gc():setColorRGB(255,0,255)
-	elseif theColor == "gold" then platform.gc():setColorRGB(255,215,0)
-	elseif theColor == "gray" or theColor == "grey" then platform.gc():setColorRGB(127,127,127)
-	elseif theColor == "green" then platform.gc():setColorRGB(0,128,0)
-	elseif theColor == "lightblue" then platform.gc():setColorRGB(173,216,230)
-	elseif theColor == "lightgreen" then platform.gc():setColorRGB(144,238,144)
-	elseif theColor == "magenta" then platform.gc():setColorRGB(255,0,255)
-	elseif theColor == "maroon" then platform.gc():setColorRGB(128,0,0)
-	elseif theColor == "navyblue" then platform.gc():setColorRGB(159,175,223)
-	elseif theColor == "orange" then platform.gc():setColorRGB(255,165,0)
-	elseif theColor == "palegreen" then platform.gc():setColorRGB(152,251,152)
-	elseif theColor == "pink" then platform.gc():setColorRGB(255,192,203)
-	elseif theColor == "purple" then platform.gc():setColorRGB(128,0,128)
-	elseif theColor == "red" then platform.gc():setColorRGB(255,0,0)
-	elseif theColor == "royalblue" then platform.gc():setColorRGB(65,105,225)
-	elseif theColor == "salmon" then platform.gc():setColorRGB(250,128,114)
-	elseif theColor == "seagreen" then platform.gc():setColorRGB(46,139,87)
-	elseif theColor == "silver" then platform.gc():setColorRGB(192,192,192)
-	elseif theColor == "turquoise" then platform.gc():setColorRGB(64,224,208)
-	elseif theColor == "violet" then platform.gc():setColorRGB(238,130,238)
-	elseif theColor == "white" then platform.gc():setColorRGB(255,255,255)
-	elseif theColor == "yellow" then platform.gc():setColorRGB(255,255,0)
-	end	
+	myGC:setColorRGB(0,0,0) -- set black as default if nothing else valid is found
+	if type(theColor) == "string" then
+		theColor = string.lower(theColor)
+		if type(Color[theColor]) == "table" then myGC:setColorRGB(unpack(Color[theColor])) end
+	elseif type(theColor) == "table" then
+		myGC:setColorRGB(unpack(theColor))
+	end
 end
 
 function verticalBar(x)
-	platform.gc():drawLine(x,1,x,platform.window:height())
+	myGC:drawLine(x,1,x,platform.window:height())
 end
 
 function horizontalBar(y)
-	platform.gc():drawLine(1,y,platform.window:width(),y)
+	myGC:drawLine(1,y,platform.window:width(),y)
 end
 
 function drawSquare(x,y,l)
-	platform.gc():drawPolyLine({(x-l/2),(y-l/2), (x+l/2),(y-l/2), (x+l/2),(y+l/2), (x-l/2),(y+l/2), (x-l/2),(y-l/2)})
+	myGC:drawPolyLine({(x-l/2),(y-l/2), (x+l/2),(y-l/2), (x+l/2),(y+l/2), (x-l/2),(y+l/2), (x-l/2),(y-l/2)})
 end
 
 function drawRoundRect(x,y,width,height,radius)
 	x = x-width/2  -- let the center of the square be the origin (x coord)
 	y = y-height/2 -- same for y coord
 	if radius > height/2 then radius = height/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max radius)
-	platform.gc():drawLine(x + radius, y, x + width - (radius), y);
-	platform.gc():drawArc(x + width - (radius*2), y + height - (radius*2), radius*2, radius*2, 270, 90);
-	platform.gc():drawLine(x + width, y + radius, x + width, y + height - (radius));
-	platform.gc():drawArc(x + width - (radius*2), y, radius*2, radius*2,0,90);
-	platform.gc():drawLine(x + width - (radius), y + height, x + radius, y + height);
-	platform.gc():drawArc(x, y, radius*2, radius*2, 90, 90);
-	platform.gc():drawLine(x, y + height - (radius), x, y + radius);
-	platform.gc():drawArc(x, y + height - (radius*2), radius*2, radius*2, 180, 90);
+	myGC:drawLine(x + radius, y, x + width - (radius), y);
+	myGC:drawArc(x + width - (radius*2), y + height - (radius*2), radius*2, radius*2, 270, 90);
+	myGC:drawLine(x + width, y + radius, x + width, y + height - (radius));
+	myGC:drawArc(x + width - (radius*2), y, radius*2, radius*2,0,90);
+	myGC:drawLine(x + width - (radius), y + height, x + radius, y + height);
+	myGC:drawArc(x, y, radius*2, radius*2, 90, 90);
+	myGC:drawLine(x, y + height - (radius), x, y + radius);
+	myGC:drawArc(x, y + height - (radius*2), radius*2, radius*2, 180, 90);
 end
 
 function fillRoundRect(x,y,wd,ht,radius)  -- wd = width and ht = height -- renders badly when transparency (alpha) is not at maximum >< will re-code later
 	if radius > ht/2 then radius = ht/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max radius)
-    platform.gc():fillPolygon({(x-wd/2),(y-ht/2+radius), (x+wd/2),(y-ht/2+radius), (x+wd/2),(y+ht/2-radius), (x-wd/2),(y+ht/2-radius), (x-wd/2),(y-ht/2+radius)})
-    platform.gc():fillPolygon({(x-wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y+ht/2), (x-wd/2+radius),(y+ht/2), (x-wd/2+radius),(y-ht/2)})
+    myGC:fillPolygon({(x-wd/2),(y-ht/2+radius), (x+wd/2),(y-ht/2+radius), (x+wd/2),(y+ht/2-radius), (x-wd/2),(y+ht/2-radius), (x-wd/2),(y-ht/2+radius)})
+    myGC:fillPolygon({(x-wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y+ht/2), (x-wd/2+radius),(y+ht/2), (x-wd/2+radius),(y-ht/2)})
     x = x-wd/2  -- let the center of the square be the origin (x coord)
 	y = y-ht/2 -- same
-	platform.gc():fillArc(x + wd - (radius*2), y + ht - (radius*2), radius*2, radius*2, 1, -91);
-    platform.gc():fillArc(x + wd - (radius*2), y, radius*2, radius*2,-2,91);
-    platform.gc():fillArc(x, y, radius*2, radius*2, 85, 95);
-    platform.gc():fillArc(x, y + ht - (radius*2), radius*2, radius*2, 180, 95);
+	myGC:fillArc(x + wd - (radius*2), y + ht - (radius*2), radius*2, radius*2, 1, -91);
+    myGC:fillArc(x + wd - (radius*2), y, radius*2, radius*2,-2,91);
+    myGC:fillArc(x, y, radius*2, radius*2, 85, 95);
+    myGC:fillArc(x, y + ht - (radius*2), radius*2, radius*2, 180, 95);
 end
 
-function drawLinearGradient(r1,g1,b1,r2,g2,b2)
+function drawLinearGradient(color1,color2)
+	-- syntax would be : color1 and color2: {r,g,b} 
  	-- not sure if it's a good idea...
+ 	-- todo with unpack(color1) and unpack(color2)
+end
+
+function clearWindow(theColor)
+    setColor(theColor) -- will handle both strings like "red" and direct colors like {255,0,0}
+    myGC:fillRect(0, 0, platform.window:width(), platform.window:height())
 end
 
 function on.paint(gc)
-	setColor("red")
+	myGC = gc -- very important line !
+	setColor("red") -- is the same as gc:setColorRGB(unpack(Color["red"])) or setColor({255,0,0})
 	drawPoint(50,50)
 	drawCircle(150,50,20)
 	drawSquare(200,60,30)
@@ -146,7 +166,7 @@ function on.paint(gc)
 	verticalBar(20)
 	horizontalBar(40)
 --	screenRefresh()
---  fillScreen("color") - TODO
+--  clearWindow({0, 0, 255}) - will fill the screen with the given color. Here, blue.
 end
 
 
