1dea1f1a769f33d9e904d6d361e5d53189aa2241
Adrien Bertrand <adrienbertrand@msn.com>
2011-08-28 12:03:41 +0200

    device table + optimizations + readme file

--- BetterLuaAPI.lua
+++ BetterLuaAPI.lua
@@ -1,9 +1,26 @@
 -- BetterLuaAPI for TI-Nspire
--- Version 2.1 (Aug 7th 2011)
+-- Version 2.2 (Aug 28th 2011)
 -- Adriweb 2011 -- Thanks to Jim Bauwens and Levak
 -- adrienbertrand@msn.com
 -- Put all this or some of this (be careful, though, some functions use each other) in your code and thank me ;)
 -- Remember to put "myGC = gc" in the  on.paint(gc)  function.
+
+
+device = { api, hasColor, isCalc, theType, lang }
+device.api = platform.apilevel
+device.hasColor = platform.isColorDisplay()
+device.lang = locale.name()
+
+-- See the use of that device table in the on.paint function
+
+function on.create()
+	on.resize()
+end
+
+function on.resize()
+    device.isCalc = (platform.window:width() < 320)
+    device.theType = platform.isDeviceModeRendering() and "handheld" or "software"
+end
 
 
 Color = {
@@ -40,7 +57,7 @@ Color = {
 Color.mt = {__index = function () return {0,0,0} end}
 setmetatable(Color,Color.mt)
 
-function copyTable(t)
+local function copyTable(t)
   local t2 = {}
   for k,v in pairs(t) do
     t2[k] = v
@@ -48,7 +65,7 @@ function copyTable(t)
   return t2
 end
 
-function deepcopy(t) -- This function recursively copies a table's contents, and ensures that metatables are preserved. That is, it will correctly clone a pure Lua object.
+local function deepcopy(t) -- This function recursively copies a table's contents, and ensures that metatables are preserved. That is, it will correctly clone a pure Lua object.
 	if type(t) ~= 'table' then return t end
 	local mt = getmetatable(t)
 	local res = {}
@@ -62,35 +79,39 @@ function deepcopy(t) -- This function recursively copies a table's contents, and
 	return res
 end -- from http://snippets.luacode.org/snippets/Deep_copy_of_a_Lua_Table_2
 
-function test(arg)
+local function test(arg)
 	return arg and 1 or 0
 end
 
-function screenRefresh()
+local function screenRefresh()
 	return platform.window:invalidate()
 end
 
-function pww()
+local function pww()
 	return platform.window:width()
 end
 
-function pwh()
+local function pwh()
 	return platform.window:height()
 end
 
-function drawPoint(x, y)
+local function drawPoint(x, y)
 	myGC:fillRect(x, y, 1, 1)
 end
 
-function drawCircle(x, y, diameter)
+local function drawCircle(x, y, diameter)
 	myGC:drawArc(x - diameter/2, y - diameter/2, diameter,diameter,0,360)
 end
 
-function drawCenteredString(str)
+local function drawCenteredString(str)
 	myGC:drawString(str, (pww() - myGC:getStringWidth(str)) / 2, pwh() / 2, "middle")
 end
 
-function setColor(theColor)
+local function drawXCenteredString(str,y)
+	myGC:drawString(str, (pww() - myGC:getStringWidth(str)) / 2, y, "top")
+end
+
+local function setColor(theColor)
 	if type(theColor) == "string" then
 		theColor = string.lower(theColor)
 		if type(Color[theColor]) == "table" then myGC:setColorRGB(unpack(Color[theColor])) end
@@ -99,33 +120,33 @@ function setColor(theColor)
 	end
 end
 
-function verticalBar(x)
+local function verticalBar(x)
 	myGC:fillRect(x,0,1,platform.window:height())
 end
 
-function horizontalBar(y)
+local function horizontalBar(y)
 	myGC:fillRect(0,y,platform.window:width(),1)
 end
 
-function drawSquare(x,y,l)
+local function drawSquare(x,y,l)
 	myGC:drawPolyLine({(x-l/2),(y-l/2), (x+l/2),(y-l/2), (x+l/2),(y+l/2), (x-l/2),(y+l/2), (x-l/2),(y-l/2)})
 end
 
-function drawRoundRect(x,y,width,height,radius)
-	x = x-width/2  -- let the center of the square be the origin (x coord)
-	y = y-height/2 -- same for y coord
-	if radius > height/2 then radius = height/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max radius)
-	myGC:drawLine(x + radius, y, x + width - (radius), y);
-	myGC:drawArc(x + width - (radius*2), y + height - (radius*2), radius*2, radius*2, 270, 90);
-	myGC:drawLine(x + width, y + radius, x + width, y + height - (radius));
-	myGC:drawArc(x + width - (radius*2), y, radius*2, radius*2,0,90);
-	myGC:drawLine(x + width - (radius), y + height, x + radius, y + height);
-	myGC:drawArc(x, y, radius*2, radius*2, 90, 90);
-	myGC:drawLine(x, y + height - (radius), x, y + radius);
-	myGC:drawArc(x, y + height - (radius*2), radius*2, radius*2, 180, 90);
+local function drawRoundRect(x,y,wd,ht,rd)  -- wd = width, ht = height, rd = radius of the rounded corner
+	x = x-wd/2  -- let the center of the square be the origin (x coord)
+	y = y-ht/2 -- same for y coord
+	if rd > ht/2 then rd = ht/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max rd)
+	myGC:drawLine(x + rd, y, x + wd - (rd), y);
+	myGC:drawArc(x + wd - (rd*2), y + ht - (rd*2), rd*2, rd*2, 270, 90);
+	myGC:drawLine(x + wd, y + rd, x + wd, y + ht - (rd));
+	myGC:drawArc(x + wd - (rd*2), y, rd*2, rd*2,0,90);
+	myGC:drawLine(x + wd - (rd), y + ht, x + rd, y + ht);
+	myGC:drawArc(x, y, rd*2, rd*2, 90, 90);
+	myGC:drawLine(x, y + ht - (rd), x, y + rd);
+	myGC:drawArc(x, y + ht - (rd*2), rd*2, rd*2, 180, 90);
 end
 
-function fillRoundRect(x,y,wd,ht,radius)  -- wd = width and ht = height -- renders badly when transparency (alpha) is not at maximum >< will re-code later
+local function fillRoundRect(x,y,wd,ht,radius)  -- wd = width and ht = height -- renders badly when transparency (alpha) is not at maximum >< will re-code later
 	if radius > ht/2 then radius = ht/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max radius)
     myGC:fillPolygon({(x-wd/2),(y-ht/2+radius), (x+wd/2),(y-ht/2+radius), (x+wd/2),(y+ht/2-radius), (x-wd/2),(y+ht/2-radius), (x-wd/2),(y-ht/2+radius)})
     myGC:fillPolygon({(x-wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y+ht/2), (x-wd/2+radius),(y+ht/2), (x-wd/2+radius),(y-ht/2)})
@@ -137,13 +158,13 @@ function fillRoundRect(x,y,wd,ht,radius)  -- wd = width and ht = height -- rende
     myGC:fillArc(x, y + ht - (radius*2), radius*2, radius*2, 180, 95);
 end
 
-function drawLinearGradient(color1,color2)
-	-- syntax would be : color1 and color2: {r,g,b} 
- 	-- not sure if it's a good idea...
+local function drawLinearGradient(color1,color2)
+	-- syntax would be : color1 and color2 as {r,g,b}.
+ 	-- don't really know how to do that. probably converting to hue/saturation/light mode and change the hue.
  	-- todo with unpack(color1) and unpack(color2)
 end
 
-function clearWindow(theColor)
+local function clearWindow(theColor)
     setColor(theColor) -- will handle both strings like "red" and direct colors like {255,0,0}
     myGC:fillRect(0, 0, platform.window:width(), platform.window:height())
 end
@@ -154,7 +175,10 @@ function on.paint(gc)
 	drawPoint(50,50)
 	drawCircle(150,50,20)
 	drawSquare(200,60,30)
-	drawCenteredString("hello world - Adriweb here \\o/")
+	local isCalcCX = device.hasColor and " a CX." or "not a CX."
+	drawXCenteredString("You are on the " .. device.theType .. ", and it's " .. isCalcCX, 50)
+	drawCenteredString("hello world - Centered String")
+	drawXCenteredString("X-Centered String  -  Adriweb here \\o/",0.75*pwh())
 	drawRoundRect(200,160,51,51,10)
 	fillRoundRect(100,160,100,75,20)
 	verticalBar(20)
---
+++ README.md
@@ -0,0 +1,37 @@
+# BetterLuaAPI for TI-Nspire.
+
+### Made by Adrien "Adriweb" Bertrand.
+
+## How to use :
+Copy in your code the functions you want and start using them, that's all ! :-)
+Please also write in your code/readme something like "[a part of] BetterLuaAPI was used"...
+Since this is all written in Nspire-Lua so it requires that you have the OS 3.0 (or later) on your calculator. 
+The latest OS updates are available on [TI's Website](http://education.ti.com).
+
+## New functions/things brought by BetterLuaAPI :
+*__device__ table : informations about the user's device (apiversion ? , hasColor ? , isCalc ? , type of calc ? , current language ? )-
+*__Colors table__ (see _setColor(...)_)
+*__copyTable(table)__ (copies a table into another)
+*__deepcopy(table)__ (deep-copies a table into another)
+*__test(arg)__ (returns 1 if _arg_ true or 0 if _arg_ false)
+*__screenRefresh()__ same as _platform.window:invalidate()_ but shorter
+*__pww()__ same as _platform.window:width()_ but shorter
+*__pwh()__ same as _platform.window:height()_ but shorter
+*__drawPoint(x, y)__ 
+*__drawCircle(x, y, diameter)__ 
+*__drawCenteredString(str)__ 
+*__drawXCenteredString(str,y)__ 
+*__setColor(theColor)__ 
+*__verticalBar(x)__ 
+*__horizontalBar(y)__ 
+*__drawSquare(x,y,l)__ 
+*__drawRoundRect(x,y,width,height,radius)__ 
+*__fillRoundRect(x,y,wd,ht,radius)__ 
+*__clearWindow(theColor)__ 
+
+
+## Notes :
+More information about Lua programming on the TI-Nspire : [Inspired-Lua](http://www.inspired-lua.org)
+
+
+__Contact : adrienbertrand @ msn . com__
